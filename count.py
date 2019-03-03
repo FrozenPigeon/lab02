5fe473c08976ccc104c79fd9446faa71b6086cff
Kevin Chung <kevinchungcadets@gmail.com>
2019-03-03 23:43:42 +1100

    added count.py functions

--- count.py
+++ count.py
@@ -3,10 +3,22 @@
 def count_char(text):
     # TODO count the number of times each character occurs in the text
     # and print out each character along with its count
+    noDup = []
+    for i in list(text):
+        if i not in noDup:
+            noDup.append(i)
+    for i in noDup:
+        print (i, text.count(i))
     pass
 
 def count_char_insensitive(text):
     # TODO do the same as `count_char` but in a case-insensitive manner
+    noDup = []
+    for x in list(text.lower()):
+        if x not in noDup:
+            noDup.append(x)
+    for i in noDup:
+        print (i, text.lower().count(i))
     pass
 
 
@@ -15,5 +27,5 @@ def count_char_ordered(text):
     # HINT: lookup `sorted()` in the Python documentation
     
     # This task is quite difficult, so please feel free to make use of
-    # resources online (Python docs, Stack Overflow, etc.)
+    # noDupources online (Python docs, Stack Overflow, etc.)
     pass
